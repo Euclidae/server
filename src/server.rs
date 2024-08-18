bdf612399812bc41b66a49f90de3e7e72b6936e6
TypicRavager <143367005+TypicRavager@users.noreply.github.com>
2024-08-18 08:06:52 +0200

    Can now listen

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
 use std::net::{TcpListener,TcpStream};
+use std::io::Read;
+
 pub struct Server {
     address: String,
 }
@@ -10,6 +12,22 @@ impl Server {
 
     pub fn run(&self) {
         println!("Listening on {}", self.address);
-        let listener = TcpListener::bind(&self.address);
+
+        let listener = TcpListener::bind(&self.address).unwrap();
+
+        loop{
+            match listener.accept(){
+                Ok((mut stream,_))=> {
+                    let mut buffer = [0;1024];
+                    match stream.read(&mut buffer){
+                        Ok(_)=>{
+                            println!("Received a reuqest:{}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e) => println!("Failed to establish a connection: {}",e),
+                    }
+                },
+                Err(e)=>println!("Failed to establish a connection: {}",e)
+            }
+        }
     }
 }
